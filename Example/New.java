1eb47ea931b0df80baa37944ec5d536b6fa29115
padmaja123-tech <padmaja.ganipisetti@gmail.com>
2021-08-27 15:05:07 -0400

    added second and third lines

--- Example/New.java
+++ Example/New.java
@@ -4,6 +4,9 @@ public class New {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First program");
+		System.out.println("second line");
+		System.out.println("Third line");
+		
 
 	}
 
